bfb46393ace330b22b89c56f027adfda3caf74b5
amulya <amulyagaur111@gmail.com>
2019-10-25 09:34:24 +0530

    Updates

---
+++ precache-manifest.383c0a318579e5bd02d518ee2d077107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43d8ccbf2db84e285b9eeadc43bd020",
+    "url": "/react-sudoku/index.html"
+  },
+  {
+    "revision": "b6f7ca10dd3464902e4f",
+    "url": "/react-sudoku/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9788435804241a4b7640",
+    "url": "/react-sudoku/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b6f7ca10dd3464902e4f",
+    "url": "/react-sudoku/static/js/2.93d52f6f.chunk.js"
+  },
+  {
+    "revision": "9788435804241a4b7640",
+    "url": "/react-sudoku/static/js/main.26e4fcfe.chunk.js"
+  },
+  {
+    "revision": "dbb84e0631a7fb8cbce7",
+    "url": "/react-sudoku/static/js/runtime-main.73870344.js"
+  }
+]);
